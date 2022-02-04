121019d9e7fb09033bd86910ce46a31abdf34692
Gabriel Gomes de Almeida <gabrielda@.ciandt.com>
2022-02-04 09:18:03 -0300

    Creating a dinamic form using React

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
+import React, {useState} from 'react';
+
+
 const App = () => {
+
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [age, setAge] = useState('');
+    
+    const handleNameInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    }
+    
+    const handleSurnameInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSurname(event.target.value);
+    }
+    
+    const handleAgeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setAge(event.target.value);
+    }
+
   return (
     <div>
       <div>
         Nome:
-        <input type="text" />
+        <input type="text" value={name} onChange={handleNameInput} />
       </div>
 
       <div>
         Sobrenome:
-        <input type="text" />
+        <input type="text" value={surname} onChange={handleSurnameInput} />
       </div>
 
       <div>
         Idade:
-        <input type="text" />
+        <input type="text" value={age} onChange={handleAgeInput} />
       </div>
 
       <hr/>
 
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
+      Olá {name} {surname}, tudo bem?<br/>
+      Você tem {age} anos.
     </div>
   );
 }
